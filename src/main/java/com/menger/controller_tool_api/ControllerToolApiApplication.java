627a8484898b0bdc7cd5b13fb8c300c79ca3692c
Matheus Menger <matheus-menger@hotmail.com>
2025-04-03 23:05:26 -0300

    Commit inicial da aplicação Spring

---
+++ src/main/java/com/menger/controller_tool_api/ControllerToolApiApplication.java
@@ -0,0 +1,13 @@
+package com.menger.controller_tool_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControllerToolApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControllerToolApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/menger/controller_tool_api/ControllerToolApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.menger.controller_tool_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControllerToolApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
